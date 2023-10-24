e6b95e5c32b9b0d0874c2cf587824d638d34fae1
jayedmonson <143557802+jayedmonson@users.noreply.github.com>
2023-10-24 11:15:46 -0500

    Update node-example2.js

--- node-example2.js
+++ node-example2.js
@@ -1,4 +1,5 @@
 const { PI } = Math;  // private to this file
 
-area = (r) => PI * r ** 2;  // exported
-circumference = (r) => 2 * PI * r;  // exported
+exports.area = (r) => PI * r ** 2;  // exported
+exports.circumference = (r) => 2 * PI * r;  // exported
+
